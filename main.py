c5a64826b9136c3907e48169eb280d516fd9d444
lienkko <tema.nesmeyanow@yandex.ru>
2022-01-18 22:17:42 +0300

    addition games

--- main.py
+++ main.py
@@ -8,38 +8,43 @@ class Menu:
         self.menu_items = menu_items
 
     # рендер каждого пункта
-    def render(self, scr, font, active_item):
+    def render_items(self, scr, font, active_item):
         for i in self.menu_items:
             if i[5] == active_item:
                 scr.blit(font.render(i[2], 1, i[4]), i[0])
             else:
                 scr.blit(font.render(i[2], 1, i[3]), i[0])
 
+    def render_games(self, scr):
+        pygame.draw.rect(scr, (0, 0, 0), (10,10, 100, 50), border_radius=2)
+
     # цикл меню
     def menu_cyc(self):
         run = True
         menu_font = pygame.font.Font("fonts\\Arial.otf", 80)
-
+        menu_section = "items"
         while run:
             screen.fill((0, 0, 0))
             screen.blit(pygame.image.load("images\\background.jpg"), (0, 0))
-            item = -1
             mouse_pos = pygame.mouse.get_pos()
-            for i in self.menu_items:
-                if i[0][0] + i[1][0] > mouse_pos[0] > i[0][0] and i[0][1] + i[1][1] > mouse_pos[1] > i[0][1]:
-                    item = i[5]
-            self.render(screen, menu_font, item)
-
-            for menu_event in pygame.event.get():
-                if menu_event.type == pygame.QUIT:
-                    sys.exit()
-                if menu_event.type == pygame.MOUSEBUTTONDOWN and menu_event.button == 1:
-                    if item == 0:
-                        run = False
-                    if item == 1:
-                        run = False
-                    elif item == 2:
+            if menu_section == "items":
+                item = -1
+                for i in self.menu_items:
+                    if i[0][0] + i[1][0] > mouse_pos[0] > i[0][0] and i[0][1] + i[1][1] > mouse_pos[1] > i[0][1]:
+                        item = i[5]
+                self.render_items(screen, menu_font, item)
+                for menu_event in pygame.event.get():
+                    if menu_event.type == pygame.QUIT:
                         sys.exit()
+                    if menu_event.type == pygame.MOUSEBUTTONDOWN and menu_event.button == 1:
+                        if item == 0:
+                            menu_section = "games"
+                        if item == 1:
+                            run = False
+                        elif item == 2:
+                            sys.exit()
+            if menu_section == "games":
+                self.render_games(screen)
             pygame.display.flip()
 
 
@@ -69,12 +74,4 @@ size = width, height = 1280, 720
 screen = pygame.display.set_mode(size)
 menu = Menu(items)
 menu.menu_cyc()
-clock = pygame.time.Clock()
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
 pygame.quit()
